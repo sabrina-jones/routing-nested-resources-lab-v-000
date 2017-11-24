b8a9b690dae35cee8e018a632a0240472f8b1737
Sabrina Jones <sabrina.jones87@gmail.com>
2017-11-24 03:08:50 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,6 +1,14 @@
 class SongsController < ApplicationController
   def index
-    @songs = Song.all
+   if params[:artist_id]
+      if Artist.find_by(id: params[:artist_id])
+        @songs = Artist.find(params[:artist_id]).songs
+      else 
+        redirect_to artists_path
+     end
+   else
+      @songs = Song.all
+    end
   end
 
   def show
